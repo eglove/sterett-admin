4b1c5a42e47ff689d9d6ca4f698f31f75fff24cb
Ethan Glover <hello@ethang.email>
2023-12-04 07:51:24 -0600

    update builder

--- build.mjs
+++ build.mjs
@@ -3,6 +3,5 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 await projectBuilder("sterett-admin", "master", {
   preVersionBumpScripts: ["UPDATE", "PRUNE"],
   postVersionBumpScripts: ["DEDUPE", "LINT", "BUILD"],
-  isIgnoringBuild: true,
-  isIgnoringPeerDependencies: true,
+  isLibrary: false,
 });
