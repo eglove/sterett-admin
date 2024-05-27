fc26db20619b3a1213a36b480b157cc883b1fde2
Ethan Glover <hello@ethang.email>
2024-05-26 21:17:03 -0500

    dont lint

--- build.mjs
+++ build.mjs
@@ -2,6 +2,6 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-admin", "master", {
   preVersionBumpScripts: ["UPDATE"],
-  postVersionBumpScripts: ["DEDUPE", "LINT", "BUILD"],
+  postVersionBumpScripts: ["DEDUPE", "BUILD"],
   isLibrary: false,
 });
