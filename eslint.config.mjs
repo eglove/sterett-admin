931e905db1c3428f9387c2005bf2b577820682fb
Ethan <hello@ethang.email>
2024-09-27 22:24:26 -0500

    fixup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,13 @@ import config from "@ethang/eslint-config/eslint.config.js";
 import configReact from "@ethang/eslint-config/config.react.js";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(...config, ...configReact, {
+export default tseslint.config(
+  {
+    ignores: ["dist", "**/*.mjs", ".sanity", "**/*.json"]
+  },
+  ...config,
+  ...configReact,
+  {
   languageOptions: {
     parserOptions: {
       project: true,
