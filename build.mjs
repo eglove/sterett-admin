b1e3443fb748165a4457d74d9ccec6f449041abb
Ethan <hello@ethang.email>
2024-09-27 22:53:22 -0500

    pnpm

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-admin", "master", {
-  scripts: ["bun x taze latest -I", "bun lint", "bun run build"],
+  scripts: ["pnpm up -i --latest", "pnpm lint", "pnpm build"],
   isLibrary: false,
 });
