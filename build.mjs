662723338ea23930a142a00f5195ab92fab0121a
Ethan <hello@ethang.email>
2024-10-11 09:07:44 -0500

    dedupe

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 
 await projectBuilder("sterett-admin", "master", {
-  scripts: ["pnpm up -i --latest", "pnpm lint", "pnpm build"],
+  scripts: ["pnpm up -i --latest", "pnpm dedupe", "pnpm lint", "pnpm build"],
   isLibrary: false,
 });
